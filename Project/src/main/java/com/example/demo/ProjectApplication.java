cf067e7f1bd1633141819e776c9ade8c82ca872d
dinuZgit <38790356+dinuZgit@users.noreply.github.com>
2018-09-24 21:26:26 +0530

    Fault Management System

--- Project/src/main/java/com/example/demo/ProjectApplication.java
+++ Project/src/main/java/com/example/demo/ProjectApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ComponentScan("com.example")
+@EnableJpaAuditing
 public class ProjectApplication {
 
 	public static void main(String[] args) {
